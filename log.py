e7dc7507aa71cfe5f85abd9480f6cf54df306f02
irumaru <jibakomaorochi13@outlook.jp>
2022-11-03 21:39:57 +0900

    プログラム側でログをファイルに出力するように変更

--- log.py
+++ log.py
@@ -1,2 +1,12 @@
+import datetime
+
+from config import LOG_PATH
+
 def logger(message):
-    print(message)
+    # 時刻の追加
+    now = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+    message = "[%s]: %s" % (now, message)
+
+    # ファイルに出力
+    with open(LOG_PATH, 'a') as p:
+        print(message, file=p)
